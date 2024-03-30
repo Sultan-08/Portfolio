61e0960d7d715430c5ca7751b49c6a710d52eead
Sultan <128064384+Sultan-08@users.noreply.github.com>
2024-03-30 12:38:47 +0530

    Add files via upload

--- Script.js
+++ Script.js
@@ -1,34 +1,14 @@
+function sendToWhatsapp() {
+    let number = "+917887971801";
 
-let button = document.querySelector("button");
-let sticker = document.querySelector(".sticker");
-let button2 = document.querySelector("#btn2");
-let button3 = document.querySelector("#btn3");
-// let GL = document.querySelector("#GL");
-// const parent = document.querySelector(".button");
-// parent.addEventListener("click", function(e) {
-//     const child = e.target.matches(".child, .child *");
-//     sticker.style.left = "50rem";
-// });
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
 
-const getLinks = document.querySelector(".btn");
-// getLinks.addEventListener("onclick" , ()=> {
-//     button2.style.transition = "10s";
-//     button2.style.left = "38em";
-//     button3.style.transition = "10s";
-//     button3.style.left = "30em";
-// });
-function openLinks() {
-    document.getElementById("getLinks")
-    button2.style.transition = "10s";
-    button2.style.left = "38em";
-    button3.style.transition = "10s";
-    button3.style.left = "30em";
+    var url = "https://wa.me/" + number + "?text="
+    + "Name : " +name+ "%0a"
+    + "Email : " +email+ "%0a"
+    + "Message : " +message+ "%0a%0a";
+
+    window.open(url, '_blank').focus();
 }
-
-    function openForm() {
-        document.getElementById("myForm").style.display = "block";
-    }
-
-    function closeForm() {
-        document.getElementById("myForm").style.display = "none";
-    }
